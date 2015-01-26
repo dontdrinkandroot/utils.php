343429998a4a8f9a77979ff260678278a3320bba
Philip Washington Sorst <philip@sorst.net>
2015-01-26 19:25:01 +0100

    Adding TypeUtils

---
+++ src/Net/Dontdrinkandroot/Utils/TypeUtils.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Net\Dontdrinkandroot\Utils;
+
+class TypeUtils
+{
+
+    /**
+     * Asserts that the input is an integerish, otherwise null will be returned.
+     *
+     * @param mixed $value The input value.
+     *
+     * @return integer|null
+     */
+    public static function integerOrNull($value)
+    {
+        $intVal = intval($value);
+        if (
+            is_object($value)
+            || strval($intVal) != $value
+            || is_bool($value)
+            || is_null($value)
+        ) {
+            return null;
+        }
+
+        return $intVal;
+    }
+}
---
+++ test/Net/Dontdrinkandroot/Utils/TypeUtilsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Net\Dontdrinkandroot\Utils;
+
+class TypeUtilsTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testIntegerOrNull()
+    {
+        $this->assertNull(TypeUtils::integerOrNull(''));
+        $this->assertNull(TypeUtils::integerOrNull('1.2'));
+        $this->assertNull(TypeUtils::integerOrNull('bla'));
+        $this->assertNull(TypeUtils::integerOrNull(null));
+        $this->assertNull(TypeUtils::integerOrNull([]));
+        $this->assertNull(TypeUtils::integerOrNull(['b', 'la']));
+        $this->assertNull(TypeUtils::integerOrNull(true));
+        $this->assertNull(TypeUtils::integerOrNull(false));
+
+        $this->assertEquals(0, TypeUtils::integerOrNull(0));
+        $this->assertEquals(0, TypeUtils::integerOrNull(""));
+
+        $this->assertEquals(1, TypeUtils::integerOrNull(1));
+        $this->assertEquals(1, TypeUtils::integerOrNull("1"));
+    }
+}
