e388e6ba7fd36a24d2b13f2a3daa28412f2769c9
Philip Washington Sorst <philip@sorst.net>
2017-02-24 19:24:22 +0100

    Moving Exceptions and BaseService to doctrine.php

--- src/Exception/NoResultFoundException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dontdrinkandroot\Exception;
-
-use Exception;
-
-class NoResultFoundException extends Exception
-{
-}
--- src/Exception/TooManyResultsException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dontdrinkandroot\Exception;
-
-use Exception;
-
-class TooManyResultsException extends Exception
-{
-}
